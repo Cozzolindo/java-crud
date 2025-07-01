d56d04c57e17bfacb9cb67429c5add05c8aa8a45
Cozzozzo <cozzolinocarlos@gmail.com>
2025-06-30 23:19:20 -0300

    Added API to search books by ID

--- crud-angular/crud_angular_spring/crud-spring/src/main/java/com/carlos/crud_spring/controller/BooksController.java
+++ crud-angular/crud_angular_spring/crud-spring/src/main/java/com/carlos/crud_spring/controller/BooksController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,8 @@ import com.carlos.crud_spring.repository.BooksRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -27,6 +30,14 @@ public class BooksController {
   public List<Books> booksList(){
     return booksRepository.findAll();
   }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Books> findBooksById(@PathVariable Long id){
+    return booksRepository.findById(id)
+    .map(record -> ResponseEntity.ok().body(record))
+    .orElse(ResponseEntity.notFound().build());
+  }
+
 
   @PostMapping
   public ResponseEntity<Books> create(@RequestBody Books book) {
