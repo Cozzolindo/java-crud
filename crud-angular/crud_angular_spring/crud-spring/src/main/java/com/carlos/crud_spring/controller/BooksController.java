04236fee8a0bb65cb3ece2d5a2ce5a6477ef517c
Cozzozzo <cozzolinocarlos@gmail.com>
2025-07-14 14:42:31 -0300

    Added Back End Validators with Jakarta

--- crud-angular/crud_angular_spring/crud-spring/src/main/java/com/carlos/crud_spring/controller/BooksController.java
+++ crud-angular/crud_angular_spring/crud-spring/src/main/java/com/carlos/crud_spring/controller/BooksController.java
@@ -4,15 +4,20 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.carlos.crud_spring.model.Books;
 import com.carlos.crud_spring.repository.BooksRepository;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +29,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 @RestController
 @RequestMapping("/api/books")
 @AllArgsConstructor
+@Validated
 public class BooksController {
 
   private final BooksRepository booksRepository;
@@ -36,7 +42,7 @@ public class BooksController {
 
   // Endpoint to fget a book by ID
   @GetMapping("/{id}")
-  public ResponseEntity<Books> findBooksById(@PathVariable Long id){
+  public ResponseEntity<Books> findBooksById(@PathVariable @NotNull @Positive Long id){
     return booksRepository.findById(id)
     .map(record -> ResponseEntity.ok().body(record))
     .orElse(ResponseEntity.notFound().build());
@@ -44,7 +50,8 @@ public class BooksController {
 
   // Endpoint to create a new book
   @PostMapping
-  public ResponseEntity<Books> create(@RequestBody Books book) {
+  @ResponseStatus(HttpStatus.CREATED)
+  public ResponseEntity<Books> create(@RequestBody @Valid Books book) {
 
     return ResponseEntity.status(HttpStatus.CREATED)
             .body(booksRepository.save(book));
@@ -65,9 +72,9 @@ public class BooksController {
       .orElse(ResponseEntity.notFound().build());
   }
 
-  // Endpoint to delete a book
+  // Endpoint to hard delete a book
   @DeleteMapping("/{id}")
-  public ResponseEntity<Void> delete(@PathVariable Long id){
+  public ResponseEntity<Void> delete(@PathVariable @NotNull @Positive Long id){
     return booksRepository.findById(id)
       .map(record -> {
           booksRepository.deleteById(id);
--- crud-angular/crud_angular_spring/crud-spring/src/main/java/com/carlos/crud_spring/model/Books.java
+++ crud-angular/crud_angular_spring/crud-spring/src/main/java/com/carlos/crud_spring/model/Books.java
@@ -7,6 +7,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
@@ -18,9 +22,15 @@ public class Books {
   @JsonProperty("id")
   private Long id;
 
+  @NotNull
+  @NotBlank(message = "Book name cannot be empty")
+  @Size(min = 1, max = 200, message = "Book name must be between 1 and 200 characters")
   @Column(name = "Name", nullable = false, length = 200)
   private String name;
 
+  @NotNull
+  @NotBlank(message = "Book type cannot be empty")
+  @Size(min = 1, max = 10, message = "Book type must be between 1 and 10 characters")
   @Column(name = "Genre", nullable = false, length = 10)
   private String type;
 
